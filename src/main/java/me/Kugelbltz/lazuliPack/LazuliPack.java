ca901b134299b6d343c032dc0c6c2605c013154f
Kugelbltz <arifkalndr@gmail.com>
2025-04-13 01:01:55 +0300

    0.0.1

---
+++ src/main/java/me/Kugelbltz/lazuliPack/LazuliPack.java
@@ -0,0 +1,17 @@
+package me.Kugelbltz.lazuliPack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LazuliPack extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
